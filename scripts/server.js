80b8029575c2c759ad093e1e1c9b1c4105f820e0
Mitchel Mathias <mitchelmathias2904@gmail.com>
2025-06-12 21:41:42 -0300

    apontando arquivos na raiz

--- scripts/server.js
+++ scripts/server.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
 app.post('/email', async (req, res) => {
     try {
         const {nome,email,suaEmpresa,mensagem} = req.body;
